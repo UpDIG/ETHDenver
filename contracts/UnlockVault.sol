a25763dc09687ee6a3eaec4c6accb7a36173533d
Lena Dang <lenadang@Lenas-MacBook-Pro.local>
2018-02-17 21:28:33 -0700

    commit

--- contracts/UnlockVault.sol
+++ contracts/UnlockVault.sol
@@ -3,36 +3,35 @@ import './Ownable.sol';
 import './SOMCoin.sol';
 
 
-contract UnlockVault is SOMCoin, Ownable{
+contract UnlockVault is Ownable, SOMCoin {
 
     uint public availableFund = 0;
 
-    struct CarbondAsset {
+    struct CarbonAsset {
         uint registryID;
         string assetName;
         uint carbonAsset;
     }
 
-    CarbondAsset[] public carbonAssets;
+    CarbonAsset[] public carbonAssets;
     address public openAddress;
 
     function addOpenAddress(address _openAddress) public onlyOwner {
         openAddress = _openAddress;
     }
 
-
     modifier onlyOpenAddress() {
         require(msg.sender == openAddress);
+        _;
     }
 
 
     function unlock_reserve(string _assetName, uint _carbonAsset) public onlyOwner {
-        if (_openAddress == msg.sender) revert;
-        carbonAssets.push(CarbonAsset(carbonAsset.length, _assetName, _carbonAsset));
+        carbonAssets.push(CarbonAsset(carbonAssets.length, _assetName, _carbonAsset));
         availableFund = availableFund + _carbonAsset;
         allowance(msg.sender, msg.sender);
         increaseApproval(msg.sender, _carbonAsset);
-        transferFrom(openAddress, _carbonAsset );
+        transferFrom(msg.sender, openAddress, _carbonAsset );
     }
 
 
