6509b980df63ed8c3f6028e1de110b0d85a387a5
Rob Stupay <rob@stupay.com>
2018-02-18 02:16:17 +0100

    Carbon loan update

--- contracts/CarbonLoan.sol
+++ contracts/CarbonLoan.sol
@@ -1,26 +1,18 @@
 pragma solidity ^0.4.17;
-import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
-import 'zeppelin-solidity/contracts/math/SafeMath.sol';
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
-import 'dharma/contracts/TermsContract.sol'
+import './Ownable.sol';
+import './SOMCoin.sol';
 
-contract CarbonLoan is StandardToken, Ownable{ 
-    enum State {RequestingLoan, LoanApproved, LoanReceived, LoanPaid}
+contract CarbonLoan is SOMCoin { 
 
-
-    function isLoanState(Loan storage self, LoanState state) returns (bool) {
-    updateCurrentLoanState(self);
-    return self.state == state;
-  }
-
-    //Event declaration
     event NewLoanRequested(uint id);
     event LoanApproved(uint id);
     event PaidBack(uint id);
+    
+    uint public availableFund = totalSupply;
 
     struct DebtorStruct {
         uint registryID;
-        address[] loan_cosigners; //Distributor co-sign the loan to payback on the debtor behalf
+        //address[] loan_cosigners; //Distributor co-sign the loan to payback on the debtor behalf
         uint numloans;
         uint owe;
         uint paid;
@@ -28,51 +20,35 @@ contract CarbonLoan is StandardToken, Ownable{
     }
 
     mapping (address => DebtorStruct) public debtors;
-    address[] public DebtorIndex;
+    address[] public DebtorIndexes;
 
     function isRegisteredDebtor(address debtor_address) public constant returns(bool isIndeed) {
-       if (providerIndex.length == 0) return false;
-       return (DebtorIndex[DebtorStruct[debtor_address].registryID] == debtor_address);
-   }
+       if (DebtorIndexes.length == 0) return false;
+       return (DebtorIndexes[debtors[debtor_address].registryID] == debtor_address);
+    }
 
-   function requestCosignLoan(address)
-
-    function newDebtorSignup() internal {
+    function newDebtorSignup() public {
         if (msg.sender == address(0)) revert();
         if (isRegisteredDebtor(msg.sender)) revert();
+        debtors[msg.sender].registryID = DebtorIndexes.push(msg.sender) - 1 ;
         debtors[msg.sender].numloans = 0;
         debtors[msg.sender].owe = 0;
         debtors[msg.sender].paid = 0;
-        debtors[msg.sender].canborrow = 0;
+        debtors[msg.sender].canborrow = true;
         
-    function requestloan()
-
-    function approveloan()
-
-
-
-
-        uint debtorid  = debtors.push(debtor_address, 0, 0, 0, true) - 1;
-        idtoDebtor[id] = debtor_address;
-
-
-    }
-
-    
-    function _createNewLoan(address debtor, uint amount, uint duration) internal onlyOwner {
-        uint loanid = loans.push(Loan(debtor, amount, duration)) - 1;
-        loantoDebtor[loanid] = debtor;
-
-        
-        lo
-
-
-        Loan currentLoan;
-
-        NewLoan(numLoans - 1);
-
     }
 
 
+    function approveloan(address debtor_address) public returns (bool success){
+        if (!isRegisteredDebtor(debtor_address)) revert();
+        //uint registryID = DebtorIndexes[debtor_address];
+        // uint owe = debtors[debtor_address].owe;
+        // uint paid = debtors[debtor_address].paid;
+        bool canborrow = debtors[debtor_address].canborrow;
+        if (! canborrow) return false;
+        Transfer(msg.sender, debtor_address, 5);
+        availableFund = availab
+        return true;
+    }
 
 }
